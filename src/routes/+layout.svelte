9e7968045660b40eeb026cf621e513773ae95866
avshyz <avishay@grip.security>
2023-06-01 15:13:36 +0200

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+    
+    export let data: LayoutData;
+</script>
+
+<nav>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/character/1">Character 1</a></li>
+      <li><a href="/settings">Settings</a></li>
+    </ul>
+  </nav>
+  
+  <slot />
---
+++ src/routes/character/[characterId]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<div>
+    Hello! This is the character page
+</div>
