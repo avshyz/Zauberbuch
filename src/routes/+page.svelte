46d3d27df9e80e41465bdfe970aaed835e86f5eb
avshyz <avishay@grip.security>
2023-06-01 22:54:21 +0200

    preps for state write

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,17 @@
+<script lang="ts" context="module">
+  import { BaseDirectory, createDir, readTextFile, writeTextFile, } from '@tauri-apps/api/fs';
+</script>
+
 <script lang="ts">
 	import type { PageData } from "./$types";
-    export let data: PageData;
+	export let data: PageData;
 </script>
 
 <h1>Character selection screen!</h1>
 
 <ul>
     {#each data.characters as c}
-        <li><a href={`/character/${c.id}`}>{c.name}</a></li>
+			<li><a href={`/character/${c.id}`}>{c.name}</a></li>
     {/each}
     <li>NEW CHARACTER</li>
 </ul>
