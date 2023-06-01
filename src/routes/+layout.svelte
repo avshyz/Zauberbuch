5de234d150b9849db3fb534ead8a0f44736803d2
avshyz <avishay@grip.security>
2023-06-01 15:18:35 +0200

    Remove autogen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,3 @@
-<script lang="ts">
-    import type { LayoutData } from './$types';
-    
-    export let data: LayoutData;
-</script>
-
 <nav>
     <ul>
       <li><a href="/">Home</a></li>
--- src/routes/character/[characterId]/+page.svelte
+++ src/routes/character/[characterId]/+page.svelte
@@ -1,9 +1,3 @@
-<script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
-</script>
-
 <div>
     Hello! This is the character page
 </div>
