0959f695a3e8039f776d747b1eaedcd2dda7d1f7
avshyz <avishay@grip.security>
2023-06-04 19:10:58 +0200

    Showing all available spells poc

--- src/routes/character/[characterId]/+page.svelte
+++ src/routes/character/[characterId]/+page.svelte
@@ -15,6 +15,37 @@
 
 <h3>Available Spells</h3>
 
-{#each data.relevantSpells as spell}
-	<p>{spell.name}</p>
-{/each}
+<table>
+	<thead>
+		<tr>
+			<th>Level</th>
+			<th>Spell</th>
+			<th>Casting Time</th>
+			<th>Range</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each data.relevantSpells as spell (spell.name)}
+			<tr>
+				<td>{spell.level}</td>
+				<td>{spell.name}</td>
+				<td>
+					{spell.casting_time}
+					{#if spell.ritual}
+						<span>(ritual)</span>
+					{/if}
+					{#if spell.reaction_trigger}
+						<span title={spell.reaction_trigger}>ℹ️</span>
+					{/if}
+				</td>
+				<td>{spell.range}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<style>
+	th {
+		text-align: left;
+	}
+</style>
