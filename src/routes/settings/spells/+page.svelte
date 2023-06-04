0deea77391dfe044c6fc93117d4d763eecf84440
avshyz <avishay@grip.security>
2023-06-04 18:58:25 +0200

    Manage spells page will now show all spells

--- src/routes/settings/spells/+page.svelte
+++ src/routes/settings/spells/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import spells from '$lib/assets/SrdSpells';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
 <h2>Spell Management</h2>
+
+{#each spells as spell}
+	<p>{spell.name}</p>
+{/each}
