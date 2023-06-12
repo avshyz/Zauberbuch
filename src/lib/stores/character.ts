d7a2b83c067c6ba6cf50eae155f627a6c39ece52
avshyz <avishay@grip.security>
2023-06-12 21:35:55 +0200

    fix casting of cantrips

--- src/lib/stores/character.ts
+++ src/lib/stores/character.ts
@@ -112,7 +112,7 @@ function createCharacterStore() {
 			castSpell(spell: Spell) {
 				persistentUpdate((sheet) => {
 					const newSlots: SpellSlots = [...sheet.spellSlots];
-					newSlots[spell.level - 1] -= 1;
+          if (spell.level > 0) newSlots[spell.level - 1] -= 1;
 					return { ...sheet, spellSlots: newSlots };
 				});
 			},
