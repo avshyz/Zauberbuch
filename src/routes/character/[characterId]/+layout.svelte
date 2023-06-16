2706882327d9720346750e9d009624b884314d22
avshyz <avishay@grip.security>
2023-06-16 23:49:31 +0200

    nav icons rename

--- src/routes/character/[characterId]/+layout.svelte
+++ src/routes/character/[characterId]/+layout.svelte
@@ -13,13 +13,13 @@
 		{$characterSheet.characterClass} - Level {$characterSheet.level}
 	</h3>
 	<ul class="inline">
-		<li><NavLink href={`/character/${$page.params.characterId}/`} exact>Play</NavLink></li>
+		<li><NavLink href={`/character/${$page.params.characterId}/`} exact>Cast</NavLink></li>
 		{#if bothPrepareAndLearnStages($characterSheet.characterClass)}
 			<li>
-				<NavLink href={`/character/${$page.params.characterId}/prepare`}>Prepare Spells</NavLink>
+				<NavLink href={`/character/${$page.params.characterId}/prepare`}>Prepare</NavLink>
 			</li>
 		{/if}
-		<li><NavLink href={`/character/${$page.params.characterId}/learn`}>Learn Spells</NavLink></li>
+		<li><NavLink href={`/character/${$page.params.characterId}/learn`}>Learn</NavLink></li>
 		<li><NavLink href={`/character/${$page.params.characterId}/settings`}>Manage</NavLink></li>
 	</ul>
 </Navbar>
